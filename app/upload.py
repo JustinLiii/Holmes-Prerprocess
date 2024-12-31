a0cae819bc81819d9537431fe05d60faf58360a1
Justin Li <justinlii@outlook.com>
2024-12-31 10:35:13 +0800

    fix upload

--- app/upload.py
+++ app/upload.py
@@ -1,8 +1,9 @@
 from fire import Fire
-from huggingface_hub import upload_folder
+from huggingface_hub import HfApi
 
 def main(repo_id: str, path_in_repo: str, upload_folder: str = "/app/output", msg: str = "upload dataset", token: str = ""):
-    upload_folder(repo_id = repo_id,
+    api = HfApi()
+    api.upload_folder(repo_id = repo_id,
                 folder_path = upload_folder,
                 path_in_repo=path_in_repo,
                 commit_message=msg,

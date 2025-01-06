9cda817b27b8af49dab85d4260236dcad5ed46d4
Justin Li <justinlii@outlook.com>
2025-01-06 15:24:01 +0800

    add repo_type for upload

--- app/upload.py
+++ app/upload.py
@@ -1,9 +1,10 @@
 from fire import Fire
 from huggingface_hub import HfApi
 
-def main(repo_id: str, path_in_repo: str, upload_folder: str = "/app/output", msg: str = "upload dataset", token: str = ""):
+def main(repo_id: str, path_in_repo: str, repo_type: str = "dataset", upload_folder: str = "/app/output", msg: str = "upload dataset", token: str = ""):
     api = HfApi()
     api.upload_folder(repo_id = repo_id,
+                repo_type=repo_type,
                 folder_path = upload_folder,
                 path_in_repo=path_in_repo,
                 commit_message=msg,
